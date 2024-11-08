cc1ddbd19a4cecf923418e81fcb7a3c5aae8a243
LeavesWebber <LeavesWebber@outlook.com>
2024-11-08 22:54:00 +0800

    ArticlePublish[8 周五-/1-/0 22:53]

--- DeployBlog.bat
+++ DeployBlog.bat
@@ -1,4 +1,7 @@
 @echo off
 cd /d "D:\项目文件\LeavesBlog"
-hexo g && git add . && git commit -m "ArticlePublish[%date%]" && git push
+setlocal
+set "DATE=%date:~-4%-%date:~4,2%-%date:~7,2% %time:~0,2%:%time:~3,2%"
+hexo g && git add . && git commit -m "ArticlePublish[%DATE%]" && git push
+endlocal
 pause
